66ac12dbdd555c2b027557ed41a4e2e8c6fdcd38
karolinakondziolka <karolina.kondziolka@gmail.com>
2020-07-01 23:59:59 +0200

    snake3.0

---
+++ src/main/java/com/kodilla/snake/demo/PlayerInfo.java
@@ -0,0 +1,10 @@
+package com.kodilla.snake.demo;
+
+public class PlayerInfo {
+    String name;
+    int score;
+    public PlayerInfo(String name, int score){
+        this.score = score;
+        this.name = name;
+    }
+}
